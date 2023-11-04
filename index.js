dcd3a8a2742d2a762c4716e57c0742a0e8f09bdb
jass <jasbrar080@gmail.com>
2023-11-04 12:51:14 -0700

    logical operators

--- index.js
+++ index.js
@@ -32,8 +32,12 @@ createCourse();
 async function getCourse(){
  const courses= await Course
  //.find({author:"jas"})
- //comparision operator
- .find({price:{ $gte:10,$lte:20}})
+ .find()
+ //.or([{author:"jas"},{isPublished:true}])  //logical operator
+ .and([{author:"jas"},{isPublished:true}])   // logical operator
+ //comparision operators
+ //.find({price:{ $gte:10,$lte:20}})   //comparision operators
+ .find({price:{$in :[12,100]}})    //comparision operators
 // .limit(10)
  //.sort({name:1})
  //.select({name:1,tags:1});
