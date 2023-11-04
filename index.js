446dc382d5501d1f9c43e73b98a6e00a343b5940
jass <jasbrar080@gmail.com>
2023-11-04 12:30:52 -0700

    comparision operator

--- index.js
+++ index.js
@@ -8,28 +8,35 @@ const courseSchema=new mongoose.Schema({
  author:String,
  tags:[String],
  date:{ type: Date,default: Date.now},
- isPublished:Boolean
+ isPublished:Boolean,
+ price : Number
 });
 //use model to create class for schema
 const Course=mongoose.model('Course',courseSchema);
 /*async function createCourse(){
 //creating object for class
 const course=new Course({
-  name:'javascript course',
-  author:'jas',
-  tags:['Api Service','frontend'],
-  isPublished:true
+  name:'jquery course',
+  author:'jaswinder',
+  tags:['jquery','database'],
+  isPublished:true,
+  price:12
 });
+// save to database
 const result= await course.save();
 console.log(result);
 }
-createCourse();*/
+createCourse();
+*/
 // getting documents from monngo db
 async function getCourse(){
- const courses= await Course.find({author:"jas"})
- .limit(10)
- .sort({name:1})
- .select({name:1,tags:1});
+ const courses= await Course
+ //.find({author:"jas"})
+ //comparision operator
+ .find({price:{ $gte:10,$lte:20}})
+// .limit(10)
+ //.sort({name:1})
+ //.select({name:1,tags:1});
  console.log(courses);
 }
 getCourse();
