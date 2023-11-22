fd52c3ac669b6853695935b0b3ff7f90b6f55cd6
jass <jasbrar080@gmail.com>
2023-11-21 20:10:45 -0800

    aaray of sub documents

---
+++ arrayOfSubDocument.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose'); 
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err)); 
+  const autherSchema= new mongoose.Schema(
+    { 
+       name: String,
+       bio: String, 
+       website: String 
+    }); 
+  const Author = mongoose.model('Author',autherSchema);
+  const Course = mongoose.model('Course', new mongoose.Schema({ 
+    name: String,
+    authors:[autherSchema]
+   }));
+   async function createCourse(name, authors)
+   { 
+    const course = new Course({ 
+      name,
+      authors
+   });
+    
+    const result = await course.save(); 
+    console.log(result);
+   } 
+   async function addAuthor(courseId,author){
+    const course= await Course.findById(courseId);
+    course.authors.push(author);
+    course.save();
+   }
+   async function removeAuthor(courseId,authorId){
+    const course= await Course.findById(courseId);
+    const author=course.authors.id(authorId);
+    author.remove();
+    course.save();
+   }
+   removeAuthor('655d7c57dd6cb14dec84e5e6','655d7d925dff4c2680a31ca5');
+   //addAuthor('655d7c57dd6cb14dec84e5e6',new Author({name:"benny"}));
+     
+   /* createCourse('Node Course',[
+      new Author({name:'john'}),
+      new Author({name:'Avi'})
+    ]);*/
